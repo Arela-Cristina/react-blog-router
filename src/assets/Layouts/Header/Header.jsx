e98685ed81a92a23ad2a9a94bcbfb49a9087d8d1
Arela-Cristina <instagrandine@gmail.com>
2024-12-10 15:47:43 +0100

    import Nav to Menu

--- src/assets/Layouts/Header/Header.jsx
+++ src/assets/Layouts/Header/Header.jsx
@@ -1,10 +1,12 @@
 import style from "./Header.module.css"
+import Nav from "../../../Menu-Nav/Nav"
 
 export default function Header() {
     return (
         <>
             <div className={style.container}>
                 <h1>Posts</h1>
+                <Nav />
             </div>
 
         </>
