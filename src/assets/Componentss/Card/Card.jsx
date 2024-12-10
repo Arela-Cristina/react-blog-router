7209d8297d7bba3bf17ca9de357a6e6ccb8fb727
Arela-Cristina <instagrandine@gmail.com>
2024-12-10 17:57:32 +0100

    add style

--- src/assets/Componentss/Card/Card.jsx
+++ src/assets/Componentss/Card/Card.jsx
@@ -1,8 +1,24 @@
+import style from './Card.module.css'
+import { BASE_URI } from '../../../config'
+import { Link } from 'react-router-dom'
 
-export default function Card() {
+export default function Card(posts = []) {
     return (
         <>
-            <div>Soy la Card</div>
+            <div className={style.card}>
+                <div className={style.containerImg}>
+                    <img className={style.image} src="" alt="" />
+                </div>
+                <div className={style.bodyCard}>
+                    <div>Nome</div>
+                    <div>Hashtags</div>
+                    <div>Description</div>
+                    <div className={style.buttonsCard}>
+                        <button>Save</button>
+                        <button>Elimina</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
